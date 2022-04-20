7c628f3e01764f6122b9ff4cfb5f0dceb151f9a5
Adrian Serbin <Andrey Serbin>
2022-04-20 21:15:40 +0300

    initial commit

---
+++ src/main/java/com/nikolaev/horeca/HorecaApplication.java
@@ -0,0 +1,13 @@
+package com.nikolaev.horeca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikolaev/horeca/HorecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolaev.horeca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
